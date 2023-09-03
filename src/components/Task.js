a97ece4234dad9596fcd426e3fb1cc09cdfb2b6e
peter-sila1 <peter.sila@student.moringaschool.com>
2023-09-04 00:51:33 +0300

    mini project

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React from 'react';
 
-function Task() {
+const Task = ({ task, onTaskDelete }) => {
+  const handleDeleteClick = () => {
+    onTaskDelete(task.id);
+  };
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div>{task.text}</div>
+      <div className="label">{task.category}</div>
+      <button onClick={handleDeleteClick} className="delete">Delete</button>
     </div>
   );
-}
+};
 
 export default Task;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+const TaskList = ({ tasks, onTaskDelete }) => {
+  const handleTaskDelete = (id) => {
+    onTaskDelete(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onTaskDelete={handleTaskDelete} />
+      ))}
     </div>
   );
-}
+};
 
 export default TaskList;
